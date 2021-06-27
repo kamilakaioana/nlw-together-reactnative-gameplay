cb4ce33efbb8d1a3e4637278d65d0af3808d050c
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-06-26 23:45:07 -0300

    projeto nlw

---
+++ src/configs/database.ts
@@ -0,0 +1,7 @@
+//exemplo: @app:collection
+const DATABASE_NAME = "@gameplay";
+
+const COLLECTION_USERS = `${DATABASE_NAME}:user`;
+const COLLECTION_APPOINTMENTS = `${DATABASE_NAME}:appointments`;
+
+export { COLLECTION_USERS, COLLECTION_APPOINTMENTS };
